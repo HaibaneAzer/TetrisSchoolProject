e02f5605e4272cbc0a620d41f99172e46eb38219
Marcus Mardal <puk015@uib.no>
2023-03-10 01:14:47 +0100

    minor changes

--- src/test/java/no/uib/inf101/tetris/model/tetromino/TestTetromino.java
+++ src/test/java/no/uib/inf101/tetris/model/tetromino/TestTetromino.java
@@ -1,6 +1,5 @@
 package no.uib.inf101.tetris.model.tetromino;
 
-import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
